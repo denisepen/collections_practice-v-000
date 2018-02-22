1f8d48373bb4c775e9cb7c5b7d369a1252d9ab4e
Denise Pendleton <dpendl1@hotmail.com>
2018-02-22 13:51:53 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def SORT_ARRAY_CHAR_COUNT(array)
+  
+  array.sort_by {|i| i.length}
+  
+end
+
+end
